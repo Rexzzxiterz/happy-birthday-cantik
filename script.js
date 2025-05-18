e3b22902bba8e22278d927a67043dcbad3ba10ee
SilentXploit <putrarexzz@gmail.com>
2025-05-18 22:05:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 100 },
+    color: { value: "#ffb6c1" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 4 },
+    move: { enable: true, speed: 1 }
+  },
+  interactivity: {
+    events: {
+      onhover: { enable: true, mode: "repulse" }
+    }
+  }
+});
